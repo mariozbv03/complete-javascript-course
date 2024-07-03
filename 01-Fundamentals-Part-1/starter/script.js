64d70720440130cf12a74918b06a6736cb2257d5
Mario Zabava <zabavamario01@gmail.com>
2024-07-03 16:06:28 +0300

    Finished the part 1 of the javascript fundamentals

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,135 @@
+/*const country = "Romania";
+const populationRomania = 20000000;
+const continent = 'Europe';
+
+console.log(populationRomania / 2);
+
+const populationRomania ++;
+console.log(populationRomania);
+
+const populationFinland = 6000000;
+console.log (populationRomania > populationFinland);
+
+const averagePopulation = 30000000;
+console.log (populationRomania > averagePopulation);
+
+const description =  'Portugal is in Europe, and its 11 million people speak portuguese';
+
+console.log (description);
+
+const weightMark = 78;
+const heightMark = 1.69;
+
+const weightJohn = 92;
+const heightJohn = 1.95;
+
+const bmiMark = weightMark / heightMark ** 2;
+console.log(bmiMark);
+const bmiJohn = weightJohn / heightJohn ** 2;
+console.log(bmiJohn);
+
+const markHigherBmi = bmiMark > bmiJohn;
+console.log(markHigherBmi);
+
+const populationRomania = 20;
+if (populationRomania < 33) {
+    console.log(`Portugal's population is above average`)
+} else {
+    console.log(`Portugal's population is below average by 22 million`)
+}
+
+
+const weightMark = 78;
+const heightMark = 1.69;
+
+const weightJohn = 92;
+const heightJohn = 1.95;
+
+const bmiMark = weightMark / heightMark ** 2;
+console.log(bmiMark);
+const bmiJohn = weightJohn / heightJohn ** 2;
+console.log(bmiJohn);
+
+const markHigherBmi = bmiMark > bmiJohn;
+console.log(markHigherBmi);
+
+if (bmiMark > bmiJohn) {
+    console.log(`Mark's bmi ${bmiMark} is higher than John's bmi ${bmiJohn} !`)
+} else {
+    console.log(`Mark bmi ${bmiMark} is lower than John's bmi ${bmiJohn} !`)
+}
+
+const numNeighbours = Number(prompt(`How many neighbour countries does your country have?`))
+
+if (numNeighbours === 1) {
+    console.log('Only 1 border!')
+} else if (numNeighbours > 1) {
+    console.log('More than 1 border')
+} else {
+    console.log('No neighbours')
+} 
+    const country = 'Romania';
+    const language = 'english';
+    const population = 20;
+    const isIsland = false;
+
+
+    if (language === 'english' && population < 50 && !isIsland) {
+        console.log(`${country} is the perfect country for you :)`)
+    } else {
+        console.log(`${country} is not a good country for you :(`)
+    }
+
+    const averageTeamDolphins = (96 + 108 + 89) / 3;
+    console.log(averageTeamDolphins)
+    const averageTeamKoalas = (88 + 91 + 110) / 3;
+    console.log(averageTeamKoalas)
+
+    if (averageTeamDolphins > averageTeamKoalas && averageTeamDolphins >= 100) {
+        console.log (`TeamDolphins win the trophy!`)
+    } else if (averageTeamKoalas > averageTeamDolphins && averageTeamKoalas >= 100) {
+        console.log ('TeamKoalas win the trophy')
+    } else if (averageTeamKoalas === averageTeamDolphins && averageTeamKoalas >= 100 && averageTeamDolphins >= 100) {
+        console.log(`Both teams win the trophy`)
+    } else {
+        console.log (`No one wins the trophy :(`)
+    }
+
+    const language = 'Arabic';
+
+    switch(language) {
+        case 'Chinese':
+            console.log('MOST number of native speakers!')
+            break
+        case 'Spanish':
+            console.log('2nd place in number of native speakers')
+            break
+        case 'English':
+            console.log('Third language')
+            break
+        case 'Hindi':
+            console.log('4th language')
+            break
+        case 'Arabic':
+            console.log('5th language')
+            break 
+    }
+
+            const population = 130;
+            population >= 33 ? console.log('Portugals population is above average') : console.log('Portugals population is below average');
+
+*/
+   /* const bill = 275; Using if...else statements
+    let tip;
+    if (bill >= 50 && bill <= 300) {
+        tip = 15/100 * bill
+    } else {
+      tip = 20/100 * bill
+    }
+    
+    console.log(tip) */
+
+    /* const bill = 275;
+    let tip = bill >= 50 && bill <=300 ? 15/100 * bill : 20/100 * bill;
+    const totalValue = bill + tip;
+    console.log(`The bill was ${bill} and the tip was ${tip} and the total value was ${totalValue}.`) */
